de4b2093a6a4a349318f0cf4958d2754e3dcf503
hebcodee <herberth.ferreira6@gmail.com>
2024-10-27 20:33:51 -0300

    Delete imagens directory

--- imagens/mapa/Tileset/tileLucas.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="tileLucas" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="../img/tileLucas.png" width="128" height="128"/>
-</tileset>
