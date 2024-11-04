8ea71ddaf2d1770cc6049ecdade24b010275feeb
hebcodee <herberth.ferreira6@gmail.com>
2024-11-04 18:00:36 -0300

    Adicionando Personagens + Alterando variaveis para portugues

--- mapa/mapa.js
+++ mapa/mapa.js
@@ -10,7 +10,7 @@ for (let i = 0; i < colisao.length; i += 38 ){
     colisaoMapa.push (colisao.slice(i, 38 + i))
 }
 
-class Boundary {
+class Borda {
     static width = 96
     static height = 96
     constructor({posicao}){
@@ -20,23 +20,23 @@ class Boundary {
     }
 
     draw () {
-        ctx.fillStyle = "blue"
+        ctx.fillStyle = "red"
         ctx.fillRect(this.posicao.x,this.posicao.y,this.width,this.height)
     }
 }
 
-const boundaries = []
+const bordas = []
 
-const offset = {
+const desvio = {
     x: -1400,
     y: -1000
 }
 colisaoMapa.forEach((row, i) => {
     row.forEach((symbol, j) => {
         if(symbol === 101)
-        boundaries.push(new Boundary({posicao: {
-            x: j * Boundary.width + offset.x,
-            y: i * Boundary.height + offset.y
+        bordas.push(new Borda({posicao: {
+            x: j * Borda.width + desvio.x,
+            y: i * Borda.height + desvio.y
         }}))
     })
 })
@@ -45,28 +45,56 @@ colisaoMapa.forEach((row, i) => {
 const imagemMapa = new Image()
 imagemMapa.src= "../imagens/mapa/mapaSociedadeWeb.png"
 
-const imagemUp= new Image()
-imagemUp.src= "../imagens/personagens/lucas/lucasCima.png"
+//Iago
+const iagoCimaImagem= new Image()
+iagoCimaImagem.src= "../imagens/personagens/iago/iagoCima.png"
+const iagoEsquerdaImagem= new Image()
+iagoEsquerdaImagem.src= "../imagens/personagens/iago/iagoEsquerda.png"
+const iagoBaixoImagem= new Image()
+iagoBaixoImagem.src= "../imagens/personagens/iago/iagoBaixo.png"
+const iagoDireitaImagem= new Image()
+iagoDireitaImagem.src= "../imagens/personagens/iago/iagoDireita.png"
 
-const imagemLeft= new Image()
-imagemLeft.src= "../imagens/personagens/lucas/lucasEsquerda.png"
+//Heb
+const hebCimaImagem= new Image()
+hebCimaImagem.src= "../imagens/personagens/heb/hebCima.png"
+const hebEsquerdaImagem= new Image()
+hebEsquerdaImagem.src= "../imagens/personagens/heb/hebEsquerda.png"
+const hebBaixoImagem= new Image()
+hebBaixoImagem.src= "../imagens/personagens/heb/hebBaixo.png"
+const hebDireitaImagem= new Image()
+hebDireitaImagem.src= "../imagens/personagens/heb/hebDireita.png"
 
-const imagemDown= new Image()
-imagemDown.src= "../imagens/personagens/lucas/lucasBaixo.png"
+//Nathan
+const nathanCimaImagem= new Image()
+nathanCimaImagem.src= "../imagens/personagens/nathan/nathanCima.png"
+const nathanEsquerdaImagem= new Image()
+nathanEsquerdaImagem.src= "../imagens/personagens/nathan/nathanEsquerda.png"
+const nathanBaixoImagem= new Image()
+nathanBaixoImagem.src= "../imagens/personagens/nathan/nathanBaixo.png"
+const nathanDireitaImagem= new Image()
+nathanDireitaImagem.src= "../imagens/personagens/nathan/nathanDireita.png"
 
-const imagemRight= new Image()
-imagemRight.src= "../imagens/personagens/lucas/lucasDireita.png"
+//Daniel
+// const danielnCimaImagem= new Image()
+// danielCimaImagem.src= "../imagens/personagens/daniel/danielCima.png"
+// const danielEsquerdaImagem= new Image()
+// danielEsquerdaImagem.src= "../imagens/personagens/daniel/danielEsquerda.png"
+// const danielBaixoImagem= new Image()
+// danielBaixoImagem.src= "../imagens/personagens/daniel/danielBaixo.png"
+// const danielDireitaImagem= new Image()
+// danielDireitaImagem.src= "../imagens/personagens/daniel/danielDireita.png"
 
 class Sprite {
     constructor({ posicao, velocidade, imagem, quadros = { max: 1 }, sprites }) {
         this.posicao = posicao
         this.imagem = imagem
-        this.quadros = {...quadros, val: 0, elapsed: 0}
+        this.quadros = {...quadros, val: 0, intervalo: 0}
         this.imagem.onload = () => {
             this.width = this.imagem.width / this.quadros.max
             this.height = this.imagem.height
         }
-        this.moving = false
+        this.movimento = false
         this.sprites = sprites
     }
 
@@ -83,11 +111,12 @@ class Sprite {
             this.imagem.height
         )
 
-        if (!this.moving) return
+        if (!this.movimento) return
 
-        if (this.quadros.max > 1) {this.quadros.elapsed++}
+        if (this.quadros.max > 1) {this.quadros.intervalo++}
 
-        if (this.quadros.elapsed % 10 === 0){
+        //Intervalo Animacao
+        if (this.quadros.intervalo % 10 === 0){
             if (this.quadros.val < this.quadros.max - 1) this.quadros.val++
             else this.quadros.val = 0   
     }
@@ -95,8 +124,8 @@ class Sprite {
 }
 
 const deslocamento = {
-    x: offset.x,
-    y: offset.y
+    x: desvio.x,
+    y: desvio.y
 }
 
 const mapa = new Sprite({
@@ -112,13 +141,13 @@ const jogador = new Sprite({
         x: canvas.width / 2 - 128 / 4 / 2,
         y: canvas.height / 2 - 32 / 2
     },
-    imagem: imagemDown,
+    imagem: nathanBaixoImagem,
     quadros: {max: 4},
     sprites: {
-        up: imagemUp,
-        left: imagemLeft,
-        down: imagemDown,
-        right: imagemRight
+        up: nathanCimaImagem,
+        left: nathanEsquerdaImagem,
+        down: nathanBaixoImagem,
+        right: nathanDireitaImagem
     }
 })
 
@@ -134,7 +163,7 @@ const teclas = {
 
 
 
-const objetosMoveis = [mapa, ...boundaries]
+const objetosMoveis = [mapa, ...bordas]
 
 function rectangularCollision({rectangle1, rectangle2}) {
     return(rectangle1.posicao.x + rectangle1.width >= rectangle2.posicao.x &&
@@ -146,103 +175,103 @@ function rectangularCollision({rectangle1, rectangle2}) {
 function animacao() {
     window.requestAnimationFrame(animacao) 
     mapa.desenhar()
-    boundaries.forEach(bondary =>{
-        bondary.draw()
+    bordas.forEach(borda =>{
+        borda.draw()
     })
     
     jogador.desenhar()
 
     //Movimentacao + Colisão
-    let moving = true  
-    jogador.moving = false
+    let movimento = true  
+    jogador.movimento = false
 
     if (teclas.w.pressionada && ultimaTecla === "w") {
-        jogador.moving = true
+        jogador.movimento = true
         jogador.imagem = jogador.sprites.up
-        for (let i = 0; i < boundaries.length; i++) {
-            const bondary = boundaries[i]
+        for (let i = 0; i < bordas.length; i++) {
+            const borda = bordas[i]
             if(rectangularCollision({
                 rectangle1: jogador,
-                rectangle2: {...bondary, posicao:{
-                    x: bondary.posicao.x,
-                    y: bondary.posicao.y + 3
+                rectangle2: {...borda, posicao:{
+                    x: borda.posicao.x,
+                    y: borda.posicao.y + 3
                 }}
             })) {
-                moving = false
+                movimento = false
                 break
             }
         }
 
-        if(moving)
+        if(movimento)
         objetosMoveis.forEach(movable => {movable.posicao.y += 3})}
 
         
     else if (teclas.a.pressionada && ultimaTecla === "a") {
-        jogador.moving = true
+        jogador.movimento = true
         jogador.imagem = jogador.sprites.left
 
-        for (let i = 0; i < boundaries.length; i++) {
-            const bondary = boundaries[i]
+        for (let i = 0; i < bordas.length; i++) {
+            const borda = bordas[i]
             
             if(rectangularCollision({
                 rectangle1: jogador,
-                rectangle2: {...bondary, posicao:{
-                    x: bondary.posicao.x + 3,
-                    y: bondary.posicao.y
+                rectangle2: {...borda, posicao:{
+                    x: borda.posicao.x + 3,
+                    y: borda.posicao.y
                 }}
             })) {
-                moving = false
+                movimento = false
                 break
             }
         }
 
-        if(moving)
+        if(movimento)
         objetosMoveis.forEach(movable => {movable.posicao.x += 3})}
 
 
     else if (teclas.s.pressionada && ultimaTecla === "s") {
-        jogador.moving = true
+        jogador.movimento = true
         jogador.imagem = jogador.sprites.down
 
-        for (let i = 0; i < boundaries.length; i++) {
-            const bondary = boundaries[i]
+        for (let i = 0; i < bordas.length; i++) {
+            const borda = bordas[i]
             
             if(rectangularCollision({
                 rectangle1: jogador,
-                rectangle2: {...bondary, posicao:{
-                    x: bondary.posicao.x,
-                    y: bondary.posicao.y - 3
+                rectangle2: {...borda, posicao:{
+                    x: borda.posicao.x,
+                    y: borda.posicao.y - 3
                 }}
             })) {
-                moving = false
+                movimento = false
                 break
             }
         }
 
-        if(moving)
+        if(movimento)
         objetosMoveis.forEach(movable => {movable.posicao.y -= 3})}
 
 
     else if (teclas.d.pressionada && ultimaTecla === "d") {
-        jogador.moving = true
+        jogador.movimento = true
         jogador.imagem = jogador.sprites.right
 
-        for (let i = 0; i < boundaries.length; i++) {
-            const bondary = boundaries[i]
+        for (let i = 0; i < bordas.length; i++) {
+            const borda = bordas[i]
             
             if(rectangularCollision({
                 rectangle1: jogador,
-                rectangle2: {...bondary, posicao:{
-                    x: bondary.posicao.x - 3,
-                    y: bondary.posicao.y
+                rectangle2: {...borda, posicao:{
+                    x: borda.posicao.x - 3,
+                    y: borda.posicao.y
                 }}
             })) {
-                moving = false
+                movimento = false
                 break
             }
         }
 
-        if(moving)
+        if(movimento)
         objetosMoveis.forEach(movable => {movable.posicao.x -= 3})}
 }
 animacao()
