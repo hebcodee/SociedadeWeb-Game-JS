6c328e5d856b255f200c27e63e91330358a5517a
hebcodee <herberth.ferreira6@gmail.com>
2024-11-06 18:45:12 -0300

    adicao de sprite e mudanca de mapa

---
+++ imagens/mapa/Tileset/tileGrama2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tileGrama2" tilewidth="32" tileheight="32" tilecount="770" columns="35">
+ <image source="../img/tileGrama2.png" width="1120" height="704"/>
+</tileset>
---
+++ imagens/mapa/Tileset/tileMoveis.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tileMoveis" tilewidth="32" tileheight="32" tilecount="12" columns="3">
+ <image source="../img/tileMoveis.png" width="96" height="144"/>
+</tileset>
